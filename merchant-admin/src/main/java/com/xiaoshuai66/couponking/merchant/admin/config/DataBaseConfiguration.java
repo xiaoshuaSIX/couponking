3022542286db140b0709970689ce729506940c52
zhaoshuai <xs2357377358@163.com>
2025-02-09 22:51:29 +0800

    feature: 开发用户上下文带吗，数据持久层配置类

---
+++ merchant-admin/src/main/java/com/xiaoshuai66/couponking/merchant/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,58 @@
+package com.xiaoshuai66.couponking.merchant.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Date;
+
+/**
+ * ClassName: DataBaseConfiguration
+ * Package: com.xiaoshuai66.couponking.merchant.admin.config
+ * Description:
+ *
+ * @Author 赵帅
+ * @Create 2025/2/9 22:18
+ * @Version 1.0
+ */
+@Configuration
+public class DataBaseConfiguration {
+    /**
+     * MyBatis-Plus MySQL 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    /**
+     * MyBatis-Plus 源数据自动填充类
+     */
+    @Bean
+    public MyMetaObjectHandler myMetaObjectHandler() {
+        return new MyMetaObjectHandler();
+    }
+
+    /**
+     * MyBatis-Plus 源数据自动填充类
+     */
+    static class MyMetaObjectHandler implements MetaObjectHandler {
+        @Override
+        public void insertFill(MetaObject metaObject) {
+            strictInsertFill(metaObject, "createTime", Date::new, Date.class);
+            strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+            strictInsertFill(metaObject, "delFlag", () -> 0, Integer.class);
+        }
+
+        @Override
+        public void updateFill(MetaObject metaObject) {
+            strictInsertFill(metaObject, "updateTime", Date::new, Date.class);
+        }
+    }
+}
---
+++ merchant-admin/src/main/java/com/xiaoshuai66/couponking/merchant/admin/context/UserInfoDTO.java
@@ -0,0 +1,37 @@
+package com.xiaoshuai66.couponking.merchant.admin.context;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * ClassName: UserInfoDTO
+ * Package: com.xiaoshuai66.couponking.merchant.admin.context
+ * Description: 登录用户信息实体
+ *
+ * @Author 赵帅
+ * @Create 2025/2/9 22:30
+ * @Version 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDTO {
+
+    /**
+     * 用户 ID
+     */
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 店铺编号
+     */
+    private Long shopNumber;
+}
