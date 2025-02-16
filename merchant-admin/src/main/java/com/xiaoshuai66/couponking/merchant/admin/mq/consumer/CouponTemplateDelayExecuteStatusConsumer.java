88a284861cac97134325391d66d854df2fad9a58
zhaoshuai <xs2357377358@163.com>
2025-02-16 21:08:13 +0800

    refactor: 重构优惠券模版定时结束消费者逻辑

--- merchant-admin/src/main/java/com/xiaoshuai66/couponking/merchant/admin/mq/consumer/CouponTemplateDelayExecuteStatusConsumer.java
+++ merchant-admin/src/main/java/com/xiaoshuai66/couponking/merchant/admin/mq/consumer/CouponTemplateDelayExecuteStatusConsumer.java
@@ -1,10 +1,13 @@
 package com.xiaoshuai66.couponking.merchant.admin.mq.consumer;
 
+import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.xiaoshuai66.couponking.merchant.admin.common.enums.CouponTemplateStatusEnum;
 import com.xiaoshuai66.couponking.merchant.admin.dao.entity.CouponTemplateDO;
+import com.xiaoshuai66.couponking.merchant.admin.mq.base.MessageWrapper;
+import com.xiaoshuai66.couponking.merchant.admin.mq.event.CouponTemplateDelayEvent;
 import com.xiaoshuai66.couponking.merchant.admin.service.CouponTemplateService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,19 +31,20 @@ import org.springframework.stereotype.Component;
         consumerGroup = "coupon-king_merchant-admin-service_coupon-template-delay-status_cg"
 )
 @Slf4j(topic = "CouponTemplateDelayExecuteStatusConsumer")
-public class CouponTemplateDelayExecuteStatusConsumer implements RocketMQListener<JSONObject> {
+public class CouponTemplateDelayExecuteStatusConsumer implements RocketMQListener<MessageWrapper<CouponTemplateDelayEvent>> {
 
     private final CouponTemplateService couponTemplateService;
 
     @Override
-    public void onMessage(JSONObject message) {
+    public void onMessage(MessageWrapper<CouponTemplateDelayEvent> messageWrapper) {
         // 开头打印日志，平常可 Debug 看任务参数，线上可报平安（比如消息是否消费，重新投递时间获取参数等）
-        log.info("[消费者] 优惠券模版定时执行@变更模版状态 - 执行消费逻辑，消息体：{}", message.toString());
+        log.info("[消费者] 优惠券模版定时执行@变更模版状态 - 执行消费逻辑，消息体：{}", JSON.toJSONString(messageWrapper));
 
         // 修改指定优惠券模版状态为已结束
+        CouponTemplateDelayEvent message = messageWrapper.getMessage();
         LambdaUpdateWrapper<CouponTemplateDO> updateWrapper = Wrappers.lambdaUpdate(CouponTemplateDO.class)
-                .eq(CouponTemplateDO::getShopNumber, message.getLong("shopNumber"))
-                .eq(CouponTemplateDO::getId, message.getLong("couponTemplateId"))
+                .eq(CouponTemplateDO::getShopNumber, message.getShopNumber())
+                .eq(CouponTemplateDO::getId, message.getCouponTemplateId())
                 .set(CouponTemplateDO::getStatus, CouponTemplateStatusEnum.ENDED.getStatus());
         couponTemplateService.update(updateWrapper);
     }
